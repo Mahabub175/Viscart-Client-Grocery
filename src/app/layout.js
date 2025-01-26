c197d32a22c1e14716b1d5fba032c171d6929ca2
Astro <rgrxmahabub.official@gmail.com>
2025-01-26 17:02:30 +0600

    updated

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import AntDProvider from "@/components/Shared/AntDProvider";
 import { GoogleTagManager } from "@next/third-parties/google";
+import SEOHead from "@/components/Shared/Sidebar/SEOHead";
 
 const interFont = Inter({ subsets: ["latin"], weight: ["400", "500", "700"] });
 
@@ -14,11 +15,12 @@ export const metadata = {
 const RootLayout = ({ children }) => {
   return (
     <html lang="en">
-      <GoogleTagManager gtmId="GTM-KWXB5SSR" />
+      <SEOHead />
+      <GoogleTagManager gtmId="GTM-KRWZQK5K" />
       <body className={interFont.className}>
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-KWXB5SSR"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KRWZQK5K"
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
