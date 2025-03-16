be7088eb58282e8c105ebb075d7c1e37f5427c91
Mahabub175 <rgrxmahabub.official@gmail.com>
2025-03-16 15:45:16 +0600

    updated

--- src/components/Shared/Product/DrawerCart.jsx
+++ src/components/Shared/Product/DrawerCart.jsx
@@ -114,75 +114,65 @@ const DrawerCart = ({ data, setDrawer }) => {
           {data.map((item) => (
             <div
               key={item._id}
-              className="flex flex-col lg:flex-row items-center gap-4 justify-between pb-5 mt-5 first:mt-0 border-b border-gray-300 last:border-b-0"
+              className="flex items-center gap-4 pb-5 mt-5 first:mt-0 border-b border-gray-300 last:border-b-0"
             >
-              <div className="flex items-center gap-4 relative group flex-[3]">
-                <div className="relative border rounded-xl">
-                  <Image
-                    src={formatImagePath(item.image)}
-                    alt={item.product?.name || "Product Image"}
-                    width={128}
-                    height={128}
-                    className="lg:w-[150px] lg:h-32 rounded-xl"
-                  />
-                </div>
-                <div className="w-full">
-                  <Link
-                    href={`/products/${item.slug}`}
-                    className="text-base font-normal hover:underline hover:text-black"
-                    onClick={() => setDrawer(false)}
-                  >
-                    <Tooltip placement="top" title={item.productName}>
-                      <h2 className="text-md font-semibold">
-                        {item.productName}
-                      </h2>
-                    </Tooltip>
-                    {item.variant &&
-                      ` (${item.variant.attributeCombination
-                        .map((comb) => comb.name)
-                        .join(", ")})`}
-                  </Link>
-
-                  <div className="flex justify-between items-start">
-                    <div className="mt-2">
-                      <p className="font-semibold">
-                        Quantity: {counts[item._id]}
-                      </p>
-                      <div className="flex items-center border rounded w-fit my-2">
-                        <button
-                          onClick={() =>
-                            handleQuantityChange(item._id, counts[item._id] - 1)
-                          }
-                          className="px-2 py-1 bg-gray-200 text-sm font-bold"
-                          disabled={counts[item._id] <= 1}
-                        >
-                          -
-                        </button>
-                        <span className="px-4">{counts[item._id]}</span>
-                        <button
-                          onClick={() =>
-                            handleQuantityChange(item._id, counts[item._id] + 1)
-                          }
-                          className="px-2 py-1 bg-gray-200 text-sm font-bold"
-                        >
-                          +
-                        </button>
-                      </div>
-                    </div>
-                    <MdDelete
-                      onClick={() => handleDeleteCart(item._id)}
-                      className="text-xl cursor-pointer mt-2 hover:scale-105 duration-300"
-                    />
-                  </div>
-                  <div>
-                    <p className="text-primary text-xl font-bold">
-                      {globalData?.results?.currency +
-                        " " +
-                        (item.price * counts[item._id]).toFixed(2)}
-                    </p>
-                  </div>
-                </div>
+              <div className="border rounded-xl">
+                <Image
+                  src={formatImagePath(item.image)}
+                  alt={item.product?.name || "Product Image"}
+                  width={128}
+                  height={128}
+                  className="lg:w-[120px] lg:h-32 rounded-xl"
+                />
               </div>
+
+              <div className="flex-1 space-y-2">
+                <Link
+                  href={`/products/${item.slug}`}
+                  className="text-base font-normal hover:underline hover:text-black"
+                  onClick={() => setDrawer(false)}
+                >
+                  <Tooltip placement="top" title={item.productName}>
+                    <h2 className="text-md font-semibold">
+                      {item.productName}
+                    </h2>
+                  </Tooltip>
+                  {item.variant &&
+                    ` (${item.variant.attributeCombination
+                      .map((comb) => comb.name)
+                      .join(", ")})`}
+                </Link>
+                <div className="flex items-center border rounded w-fit">
+                  <button
+                    onClick={() =>
+                      handleQuantityChange(item._id, counts[item._id] - 1)
+                    }
+                    className="px-2 py-1 bg-gray-200 text-sm font-bold"
+                    disabled={counts[item._id] <= 1}
+                  >
+                    -
+                  </button>
+                  <span className="px-4">{counts[item._id]}</span>
+                  <button
+                    onClick={() =>
+                      handleQuantityChange(item._id, counts[item._id] + 1)
+                    }
+                    className="px-2 py-1 bg-gray-200 text-sm font-bold"
+                  >
+                    +
+                  </button>
+                </div>
+                <p className="text-primary text-xl font-bold">
+                  {globalData?.results?.currency +
+                    " " +
+                    (item.price * counts[item._id]).toFixed(2)}
+                </p>
+              </div>
+
+              <MdDelete
+                onClick={() => handleDeleteCart(item._id)}
+                className="text-xl cursor-pointer hover:scale-105 duration-300"
+              />
             </div>
           ))}
         </div>
@@ -195,9 +185,9 @@ const DrawerCart = ({ data, setDrawer }) => {
       {data?.length > 0 && (
         <div
           onClick={proceedToCheckout}
-          className="hover:text-white text-white lg:text-xl absolute bottom-5 left-1/2 -translate-x-1/2 lg:-translate-x-0 lg:left-10 w-5/6 mx-auto"
+          className="hover:text-white text-white lg:text-xl absolute bottom-5 left-1/2 -translate-x-1/2 lg:-translate-x-0 lg:left-10 w-5/6 mx-auto cursor-pointer"
         >
-          <div className="flex items-center justify-between bg-primary gap-5 px-5 py-4 rounded-xl">
+          <div className="flex items-center justify-between bg-primary gap-5 px-5 py-4 rounded-xl cursor-pointer">
             <p>Proceed To Checkout</p>
             <p className="font-bold">
               {globalData?.results?.currency} {subtotal?.toFixed(2)}
