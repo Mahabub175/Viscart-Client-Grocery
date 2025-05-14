7c9ec3325de26848c8b4978bb5bc085740b163a6
Mahabub175 <rgrxmahabub.official@gmail.com>
2025-05-14 19:22:24 +0600

    Updated

--- src/utilities/configs/base_api.js
+++ src/utilities/configs/base_api.js
@@ -1,5 +1,6 @@
 export const base_url = "https://groceryapi.moonsgallerysystem.com/api/v1";
 export const base_url_image = "https://groceryapi.moonsgallerysystem.com/";
+export const base_url_client = "https://grocery.moonsgallerysystem.com";
 // export const base_url = "http://localhost:5000/api/v1";
 // export const base_url_image = "http://localhost:5000/";
 // export const base_url = "http://192.168.1.6:5000/api/v1";
